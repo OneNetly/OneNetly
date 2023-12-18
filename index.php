b8f0cf1c902bbc2869c551a8a8f9ae3cea5bbeb8
OneNetly <admin@onenetly.com>
2023-12-18 22:49:21 +0300

    ok

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>OneNetly</title>
 </head>
 <body>
 <?php include_once "header.php";?>
+
     <?php // include_once "ip.php";?>
     <?php include_once "footer.php";?>
 </body>
