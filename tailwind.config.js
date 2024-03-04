15379df57b83f3b12fbc6a4281a290ddfbde0b7e
OneNetly <admin@onenetly.com>
2024-03-04 15:42:09 +0300

    Fresh Start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,php}"],
+  content: ["./*.php"],
   theme: {
     extend: {},
   },
---
+++ themes/default/footer.php
@@ -0,0 +1,7 @@
+<div class="bg-gray-900">
+  <footer class="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div class="border-t border-gray-800 py-8 text-center text-sm text-gray-400">© <?php echo date("Y");?> - oneNetly. All rights reserved.</div>
+  </footer>
+</div>
+</body>
+</html>
