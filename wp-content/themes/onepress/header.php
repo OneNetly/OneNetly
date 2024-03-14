1fb2400fa69857302bcf38e4acfae5931ddea0ee
OneNetly <admin@onenetly.com>
2024-03-14 16:56:20 +0300

    ok

--- wp-content/themes/onepress/header.php
+++ wp-content/themes/onepress/header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="https://onenetly.com/css/output.css">
+	<script src="https://cdn.tailwindcss.com"></script>
 	<script src="//unpkg.com/alpinejs" defer></script>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
