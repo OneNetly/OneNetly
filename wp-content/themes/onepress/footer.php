36eef60327ec89a38c0fe7c643e057b39c7d26ec
OneNetly <admin@onenetly.com>
2024-03-14 22:43:56 +0300

    Next Lavel

--- wp-content/themes/onepress/footer.php
+++ wp-content/themes/onepress/footer.php
@@ -112,6 +112,14 @@
 </div>
 
 <?php wp_footer(); ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-54W25R1NPQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-54W25R1NPQ');
+</script>
 </body>
 </html>
