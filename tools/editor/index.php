58a6c7b8634eb6d5a858ac5e2ec6cdef2bc35260
OneNetly <admin@onenetly.com>
2024-03-16 01:15:41 +0300

    uu

--- tools/editor/index.php
+++ tools/editor/index.php
@@ -26,7 +26,7 @@
     <meta name="twitter:description" content="Free web hosting to launch your dream website! free domain, & reliable hosting. Start building for free today!">
     <meta name="twitter:image" content="https://onenetly.com/img/favicon.png">
 
-    <link rel="shortcut icon" type="image/jpg" href="img/favicon.png" />
+    <link rel="shortcut icon" type="image/jpg" href="https://onenetly.com/img/favicon.png" />
 	<script src="https://cdn.tailwindcss.com"></script> 
 	<script src="//unpkg.com/alpinejs" defer></script>
 
