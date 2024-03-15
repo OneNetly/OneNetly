e79974d9b8b1ffff071efd3533558a562be9339d
OneNetly <admin@onenetly.com>
2024-03-15 15:23:59 +0300

    Removed unused Files

--- tools/editor/src/common/resources/client/errors/ignored-sentry-errors.ts
+++
@@ -1,7 +0,0 @@
-export const ignoredSentryErrors = [
-  'Failed to fetch dynamically imported module',
-  "Failed to execute 'removeChild' on 'Node'",
-  "Failed to execute 'insertBefore' on 'Node'",
-  'ResizeObserver loop limit exceeded',
-  'Importing a module script failed.',
-];
--- tools/editor/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module 'style-inject' {
-  export default function styleInject(css: string): void;
-}
