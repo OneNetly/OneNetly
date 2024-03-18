de5ce8b6e1bc289e9c1a86d840dc12a9a8aa06cd
OneNetly <admin@onenetly.com>
2024-03-18 23:45:52 +0300

    Done

---
+++ hero.php
@@ -0,0 +1,24 @@
+<div class="w-full">
+    <div class="max-w-7xl mx-auto px-6 pt-16 sm:py-16 sm:pt-18 pb-10 sm:pb-18">
+        <div class="flex flex-auto flex-col md:flex-row items-center">
+
+            <div class="flex-none md:flex-1">
+                <div class="text-gray-900 text-4xl font-bold mb-10">Build, expand <br class="sm:hidden md:block lg:hidden"> your website
+                    <br>
+                    <ht class="font-bold text-purple-500"> without sweating </ht>
+                </div>
+                <span class="text-gray-700 text-xl font-normal md:pr-14">Free web hosting to launch your dream website! free domain, & reliable hosting. Start building for free today!</span>
+
+                <div class="flex flex-auto max-w-sm mt-12">
+                    <button class="bg-gray-900 px-5 py-2.5 rounded-lg ">
+                        <div class="text-white text-md md:text-lg font-semibold text-center"><a href="https://ap.onenetly.com/index.php?rp=/store/free-hosting"> Get Started</a>
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div class="flex justify-end md:flex-1">
+                <img src="img/hero.svg" alt="Hero OneNetly" class="mt-14 md:mt-0 ml-5">
+            </div>
+        </div>
+    </div>
+</div>
