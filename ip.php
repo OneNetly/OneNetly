b48daf904238c346906436f1f112dea6229b1f08
OneNetly <admin@onenetly.com>
2024-03-20 00:11:46 +0300

    ok

--- ip.php
+++ ip.php
@@ -3,11 +3,11 @@
 // Your proxycheck.io API key
 $apiKey = "160336-8915o3-794i5c-2y8800";
 
-// IP address of the user
+// User's IP address
 $userIP = $_SERVER['REMOTE_ADDR'];
 
-// Construct the API URL with all features and the user's IP address
-$url = "https://proxycheck.io/v2/$userIP?key=$apiKey&vpn=1&asn=1&risk=1&port=1&seen=1&days=7&tag=msg";
+// Construct the API URL with the user's IP address and API key
+$url = "https://proxycheck.io/v2/$userIP?key=$apiKey&vpn=1&risk=1";
 
 // Make a request to proxycheck.io API
 $response = file_get_contents($url);
@@ -15,27 +15,26 @@ $response = file_get_contents($url);
 // Decode the JSON response
 $data = json_decode($response, true);
 
-// Check if the user's IP is present in the response
+// Check if the user's IP is found in the response
 if (isset($data[$userIP])) {
-    // Check if the user's IP is flagged as a proxy
+    // Check if the IP is flagged as a proxy
     if ($data[$userIP]['proxy'] == 'yes') {
-        // Check if the user's IP is also considered bad based on its score
-        if ($data[$userIP]['score'] >= 2) {
+        // Check the risk score
+        if ($data[$userIP]['risk'] >= 50) {
             echo "You are using a VPN or Proxy and your IP is flagged as bad.";
         } else {
             echo "You are using a VPN or Proxy but your IP is not flagged as bad.";
         }
         // Print additional details
-        echo "\nASN: " . $data[$userIP]['asn'];
+        echo "\nType: " . $data[$userIP]['type'];
+        echo "\nProvider: " . $data[$userIP]['provider'];
+        echo "\nOrganisation: " . $data[$userIP]['organisation'];
         echo "\nRisk: " . $data[$userIP]['risk'];
-        echo "\nPort: " . $data[$userIP]['port'];
-        echo "\nLast Seen: " . $data[$userIP]['last_seen'];
-        echo "\nCustom Tag: " . $data[$userIP]['msg']; // Assuming 'msg' is the custom tag name
     } else {
         echo "You are not using a VPN or Proxy.";
     }
 } else {
-    // Handle the case when the user's IP is not found in the response
     echo "Unable to retrieve information for your IP address.";
 }
 ?>
+
