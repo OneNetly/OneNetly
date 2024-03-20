9d4ad1d3345e4b1820dbb779580cd329da0f24a9
OneNetly <admin@onenetly.com>
2024-03-20 19:48:17 +0300

    Ok

--- header.php
+++ header.php
@@ -82,7 +82,7 @@
 
   <script type='text/javascript' src='//pl17264611.profitablegatecpm.com/d2/70/61/d27061a80f5b3832b97510f507c12507.js'></script>
 
-  <center><script type="text/javascript">
+  <center><br><script type="text/javascript">
 	atOptions = {
 		'key' : '5c5b84f70ab47f3008ca2d0fdf9fb7d6',
 		'format' : 'iframe',
@@ -91,4 +91,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/5c5b84f70ab47f3008ca2d0fdf9fb7d6/invoke.js"></scr' + 'ipt>');
-</script></center>
+</script><br></center>
